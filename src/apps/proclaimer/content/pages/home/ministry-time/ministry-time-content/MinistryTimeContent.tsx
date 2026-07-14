a120a52be999224bb1b87f13f41e557da9b664fc
orderlyapps <orderlyapps@gmail.com>
2026-07-14 10:24:24 +1000

    refactor: update ministry time content info text styling
    Replace IonNote with IonLabel and Body component for info text. Center align text and reduce size to xs. Simplify wording from "ministry time entries" to "time entries".

--- src/apps/proclaimer/content/pages/home/ministry-time/ministry-time-content/MinistryTimeContent.tsx
+++ src/apps/proclaimer/content/pages/home/ministry-time/ministry-time-content/MinistryTimeContent.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { IonItem, IonNote } from "@ionic/react";
+import { IonItem, IonLabel } from "@ionic/react";
 import { Space } from "@ui/components/layout/space/Space";
 import { useMinistryTime, type MinistryTimeEntry } from "./hooks/useMinistryTime";
 import { AddEntryFab } from "./components/add-entry-fab/AddEntryFab";
 import { TimeEntryList } from "./components/time-entry-list/TimeEntryList";
 import { EditEntryModal } from "./components/edit-entry-modal/EditEntryModal";
 import { PioneerStats } from "./components/pioneer-stats/PioneerStats";
+import { Body } from "@ui/components/display/text/body/Body";
 
 export function MinistryTimeContent() {
   const { entries, addEntry, updateEntry, deleteEntry } = useMinistryTime();
@@ -14,11 +15,13 @@ export function MinistryTimeContent() {
   return (
     <>
       <IonItem>
-        <IonNote color="medium" className="ion-padding-horizontal">
-          Your ministry time entries and pioneer hour settings are stored locally on this device.
-          Use the settings icon above to export a copy for safekeeping or transfer to another
-          device.
-        </IonNote>
+        <IonLabel className="ion-text-center">
+          <Body size="xs" color="medium">
+            Your time entries and pioneer hour settings are stored locally on this device.
+            Use the settings icon above to export a copy for safekeeping or to transfer to another
+            device.
+          </Body>
+        </IonLabel>
       </IonItem>
       <Space size="sm" />
       <PioneerStats entries={entries} />
